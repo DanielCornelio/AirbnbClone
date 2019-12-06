ac3fa85ba061e8a932ba3c0e99c5de7fc6337749
Daniel Cornelio <jdcornelio89@gmail.com>
2019-12-05 21:09:28 -0600

    se agrega estilos al boton

--- src/components/buttons/RoundedButton.js
+++ src/components/buttons/RoundedButton.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableHighlight, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+import colors from '../../styles/colors/index';
 
 export default class RoundedButton extends Component {
   render() {
     const {text} = this.props;
     return (
-      <TouchableHighlight >
-        <Text>{text}</Text>
+      <TouchableHighlight style={styles.wrapper}>
+        <Text style={styles.buttonText}>{text}</Text>
       </TouchableHighlight>
     );
   }
@@ -18,8 +19,8 @@ RoundedButton.PropTypes = {
 };
 
 const style = StyleSheet.create({
-    wrapper:{
-
-    },
+  wrapper: {},
+  buttonText: {
+    color: colors.white,
+  },
 });
-
